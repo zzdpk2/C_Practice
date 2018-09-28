c597fce19c44049c863f35a40e43043ec19efdae
zzdpk2 <zzdpk2@126.com>
2018-09-27 23:02:00 -0500

    new files

--- Day_07/externTest/main.c
+++ Day_07/externTest/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-//声明函数,extern可有可无
+//使用前，先声明
+//声明函数,extern可有可无      
+//声明可以多次
+extern int a;
+extern int b;
 extern void test();
 
 int main(int argc, char const *argv[])
 {
     test();
+    a = 100;
+    b = 200;
+    printf("\na: %d\nb: %d\n", a, b);
+
     return 0;
 }
