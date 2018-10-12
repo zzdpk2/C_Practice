1b941f3488ded5555c41e74e0a9cf2b4a1cc1598
zzdpk2 <zzdpk2@126.com>
2018-10-12 15:12:36 -0500

    new files

---
+++ Day_07/memcpy.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+void giveValue(int TargetArr[], int length);
+void outputValue(int TargetArr[], int length);
+
+int main(int argc, char const *argv[])
+{
+    int a[10];
+    int b[10];
+    int length_a = sizeof(a) / sizeof(*a);
+    int length_b = sizeof(b) / sizeof(*b);
+
+    giveValue(a ,length_a);
+    outputValue(a, length_a);
+
+    //第三个参数是指拷贝内存的总大小
+    memcpy(b, a, 10 * sizeof(int));
+    //memcpy(b, a, sizeof(a));
+
+    //使用memcpy不要出现内存重叠
+    memcpy(&a[2], a, 5 * sizeof(int));
+
+    // 
+
+    return 0;
+}
+
+void giveValue(int TargetArr[], int length){
+
+    for(int i = 0; i < length; i++)
+        TargetArr[i] = i + 1;
+
+}
+
+void outputValue(int TargetArr[], int length){
+
+    for(int i = 0; i < length; i++)
+        printf("%d\n", TargetArr[i]);    
+
+}
+
+int main01(int argc, char const *argv[])
+{
+    //以字符串初始化，自动在末尾隐藏一个结束符'\0'
+    char p[] = "hello\0mike";
+    char buf[100];
+    printf("sizeof(p) = %lu\n", sizeof(p));
+    strncpy(buf, p, sizeof(p)); //遇到\0结束，memcpy不会，而是整段拷贝
+    printf("buf1 = %s\n", buf);
+    printf("buf1 = %s\n", buf + strlen("hello") + 1);
+    
+    memset(buf, 0, sizeof(buf));
+    //指定多长拷贝多长，不会因为\0结束
+    memcpy(buf, p, sizeof(p));
+
+    printf("buf3 = %s\n", buf);
+    printf("buf4 = %s\n", buf + strlen("hello") + 1);
+
+
+
+
+    return 0;
+}
--- Day_07/memset.c
+++ Day_07/memset.c
@@ -3,6 +3,36 @@
 
 int main(int argc, char const *argv[])
 {
+    int b[10] = {0};
+
+    //处理一些代码，把b内部元素改了
+
+    //这种方法，不可以清零数组，没有b[10]这个元素
+    //b[10] = {0};
+
+    //循环清空
+    int i = 0;
+    int n = sizeof(b) / sizeof(b[0]);
+    for(i = 0; i < n; i++)
+        b[i] = 0;
+
+    //直接到位的清空方法
+    memset(b, 0, sizeof(b));
+
+    // char str[10];
+    int str[10];
+    memset(str, 'a', sizeof(str));
+    for(i = 0; i < 10; i++)
+        // printf("%c ", str[i]);
+        printf("%d  ", str[i]);
+    printf("\n");
+
+    return 0;
+}
+
+
+int main01(int argc, char const *argv[])
+{
     int a;
     memset(&a, 0, sizeof(a));
     printf("a = %d\n", a);
@@ -10,11 +40,12 @@ int main(int argc, char const *argv[])
     // memset(&a, 10, sizeof(a));
     // printf("a1 = %d\n", a);
 
-    //中间参数虽然是整型
+    //中间参数虽然是整型，但是以字符处理
     memset(&a, 97, sizeof(a));
     printf("a = %c\n", a);
 
     int b[10];
+    //b数组清零
     memset(b, 0, sizeof(b));
     memset(b, 0, 10 * sizeof(int));
 
