8032a082fc9842a9b1ff216dabc1e4df066c5417
zzdpk2 <zzdpk2@126.com>
2018-10-13 01:00:00 -0500

    new files

---
+++ Day_07/HeapAreaOverStack.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//堆区越界
+int main(int argc, char const *argv[])
+{
+    char *p = NULL;
+
+    p = (char *)malloc(0);
+    if(p == NULL){
+        
+        printf("Allocate failed!\n");
+        return 0;
+    
+    }
+
+    strcpy(p, "MikeJiang");
+    printf("%s\n", p);
+
+    return 0;
+}
