4d127f3023a37143d202ad655b39bbd7d5e80589
zzdpk2 <zzdpk2@126.com>
2018-11-04 16:03:08 -0600

    new files

--- Day_08/usageTypedef.c
+++ Day_08/usageTypedef.c
@@ -2,8 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct Test{
+    int a;
+};
+
+typedef struct Test2{
+    int a;
+}Test2;
+
+Test2 tmp;
+
 int main(int argc, char const *argv[])
 {
-    
+    typedef int int64;
+
+    int64 a = 64;
+
     return 0;
 }
