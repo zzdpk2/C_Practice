5f2c99a9799870c08f61fa1803aea091091cf1db
zzdpk2 <zzdpk2@126.com>
2018-11-04 15:52:06 -0600

    new files

---
+++ Day_08/enumUsage.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// #define pink 0
+// #define red 1
+// #define green 2
+// #define white 3
+// #define blue 4
+// #define yellow 5
+enum Color{
+    pink, red, green, white, blue, yellow
+};
+
+int main(int argc, char const *argv[]){   
+
+    enum Color flag;
+    flag = red;
+    
+    return 0;
+}
--- Day_08/unionUsage.c
+++ Day_08/unionUsage.c
@@ -12,5 +12,14 @@ int main(int argc, char const *argv[]){
 
     printf("%lu\n", sizeof(union Test));
 
+    union Test obj;
+    printf("%p, %p, %p, %p\n", &obj, &obj.a, &obj.b, &obj.c);
+
+    obj.c = 0x44332211;
+    printf("obj.a: %x\n obj.b: %x\n obj.c: %x\n", &obj.a, &obj.b, &obj.c);
+
+    obj.a = 0xaa;
+    printf("obj.a: %x\n obj.b: %x\n obj.c: %x\n", &obj.a, &obj.b, &obj.c);
+
     return 0;
 }
